4f395cc6926f7ce35f093718ab8358433d34fd07
Ali Viettry <aviettry@ucsd.edu>
2019-08-02 14:16:44 -0700

    Finished parse_engine description

--- Friends/Randomizer/parse_engine.py
+++ Friends/Randomizer/parse_engine.py
@@ -1,1 +1,19 @@
-#   Filename: parse_engine.py
+#   Filename:       parse_engine.py
+#   
+#   Function Name:  parse_engine(argv[])
+#
+#   Function:       This function will take in an array of arguments passed in
+#                   and store up to 20 inputs in parsed_input.txt, separated by new
+#                   lines.
+#   
+#   Constraints:    The user may input anything. Each input is expected to
+#                   separated by spaces. In order to have a multiple word input,
+#                   the user should separate the words by underscores.
+#   
+#   Side Effets:    None.
+#
+#   Return:         0 if the function stored no arguments.
+#                   1 if the function stored arguments without error.
+#                   -1 if the file, parsed_input.txt, could not be written.
+
+
