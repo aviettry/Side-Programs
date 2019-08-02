6e79364c0cfd1fa2d76f7fa37da3620a0f116017
Ali Viettry <aviettry@ucsd.edu>
2019-08-02 14:29:01 -0700

    Added file header to random_function.py

--- Friends/Randomizer/random_function.py
+++ Friends/Randomizer/random_function.py
@@ -1,3 +1,16 @@
+# Filename:                 random_function.py
+#
+# Function Name:            random_function()
+#
+# Function Description:     This function will take input from a text file,
+#                           parsed_input.txt. The text file will contain at
+#                           maximum 20 arguments separated by lines. This
+#                           function will select one element at random from the
+#                           list of arguments.
+# 
+# Side Effects:             This function will output the selected element to
+#                           the console.
+#
 import random
 import sys
 
